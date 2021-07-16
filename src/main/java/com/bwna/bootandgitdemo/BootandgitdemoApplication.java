b199a85f2ed2ced8471f71f0ac00edb7fe7f1b6a
bwna <1478432593@qq.com>
2021-07-16 10:58:44 +0800

    修改了启动文件

--- src/main/java/com/bwna/bootandgitdemo/BootandgitdemoApplication.java
+++ src/main/java/com/bwna/bootandgitdemo/BootandgitdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootandgitdemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("update");
         SpringApplication.run(BootandgitdemoApplication.class, args);
     }
 
