8df679f611db3ba122b3cd3e42da2adb05fbe637
bwna <1478432593@qq.com>
2021-07-16 10:54:40 +0800

    第一次提交项目

---
+++ src/main/java/com/bwna/bootandgitdemo/BootandgitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bwna.bootandgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootandgitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootandgitdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bwna/bootandgitdemo/BootandgitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwna.bootandgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootandgitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
