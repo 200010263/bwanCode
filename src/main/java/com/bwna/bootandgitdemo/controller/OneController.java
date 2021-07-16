06c880420973afeead903e07667849698fdc44a2
bwna <1478432593@qq.com>
2021-07-16 14:39:22 +0800

    更改了注解

--- src/main/java/com/bwna/bootandgitdemo/controller/OneController.java
+++ src/main/java/com/bwna/bootandgitdemo/controller/OneController.java
@@ -2,8 +2,12 @@ package com.bwna.bootandgitdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class OneController {
 
+    public String hello(){
+        return "hello world!";
+    }
 }
