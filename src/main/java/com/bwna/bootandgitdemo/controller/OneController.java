0ccca607bdfc63d03e48fa2944f51ab029972fe6
bwna <1478432593@qq.com>
2021-07-16 16:15:52 +0800

    将代码交到github上前的最后一次修改

--- src/main/java/com/bwna/bootandgitdemo/controller/OneController.java
+++ src/main/java/com/bwna/bootandgitdemo/controller/OneController.java
@@ -1,18 +1,11 @@
 package com.bwna.bootandgitdemo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OneController {
 
     public String hello(){
-        System.out.println("gitee have update");
-        System.out.println("master commit!");
-        System.out.println("second commit!");
-        System.out.println("second commit2");
-        System.out.println("master commit2");
         return "hello world!";
     }
 }
